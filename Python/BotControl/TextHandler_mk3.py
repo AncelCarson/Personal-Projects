19d15ad59cde6390b799eda3c504daa2968d49b5
Ancel Carson <acarson@nappstech.com>
2024-10-07 22:10:32 -0400

    Implemented subprocess threads, Added custom input and print functions, Added loop for sub process queue setup, filled in docstrings

--- Python/BotControl/TextHandler_mk3.py
+++ Python/BotControl/TextHandler_mk3.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*-
 
 ### Ancel Carson
-### Created: 8/1/2020
-### UPdated: 8/1/2023
+### Created: 5/10/2024
+### UPdated: 7/10/2024
 ### Windows 11
 ### Python command line, Notepad, IDLE
 ### TextHandler_mk2.py
@@ -20,11 +20,12 @@ Classes:
 Functions:
    main: Driver of the program
    day_greeting: Sends a greeting to the user based on the time of day
-   sender: Prints any messages to discord or the command line
+   responseTest: Tests the subprocess input and output
 """
 
 # Libraries
 import os
+import queue
 from time import sleep
 from threading import Thread
 from datetime import datetime
@@ -38,31 +39,34 @@ class TextHandler:
     """Class Docstring.
 
     Variables:
+        queues (List -> Queue): Input and Output queues
         userId (str/int): Id of the user sending a message
         title (str): Title of the user
         interface (str): What interface the message was recieved from
-        answers (list -> str): Responses to messages recieved from user
+        location (str): Responses to messages recieved from user
         mode (str): Current mode of the program
-        channel (int): Discord channel a message was recieved from
         _thread (thread): Thread used to process a recieved message
+        responseQueue (Queue): Input Queue for subprograms
     
     Functions:
         messageIn: Directs an incoming message to the correct location
         responseIn: Creates a thread for processing a response
         responseProcess: A test for handling sequential responses
     """
-    def __init__(self, output_queue=None, userId=None, title="...you", interface="cmd", location = "term"):
-        self.output = output_queue
+    def __init__(self, queues=None, userId=None, title="...you", interface="cmd", location="term"):
+        self.queues = queues
         self.userId = userId
         self.title = title
         self.interface = interface
         self.location = location
-        self.mode = "waiting"
+        self.mode = "idle"
         self._thread = None
+        self.responseQueue = queue.Queue()
 
     def __call__(self):
         message = f"Thread for {self.userId} has been called"
-        self.output.put((message,"cmd","term"))
+        self.queues[1].put((message,"cmd","term"))
+        self.run()
 
     def __enter__(self):
         pass
@@ -70,96 +74,100 @@ class TextHandler:
     def __exit__(self, *exc):
         pass
 
-    def messageIn(self, message):
-        """Splits incoming messages and sorts them via keywords."""
+    def run(self):
+        """Starts the text processing thread loop for the queue"""
+        while self.mode != "kill":
+            try:
+                message = self.queues[0].get(timeout=.1)
+                self.messageIn(message)
+            except queue.Empty:
+                continue
+
+    def messageIn(self, message: str):
+        """Splits incoming messages and sorts them via keywords.
+        
+        Parameters:
+            self: TextHandler Instance
+            message: User input to start process or respond to existing process
+        """
         text=[]
 
-        if self.mode != "waiting":
-            if self.mode == "thinking":
-                text = [[f"One monent {self.title}. I am processing the last request",0]]
-                return text
-            return text
+        if self.mode == "thinking":
+            text = [f"One monent {self.title}. I am processing the last request"]
 
-        greeting = day_greeting()
+        if self.mode == "waiting":
+            self.responseQueue.put(message)
 
-        content = message.split(' ')
-        try:
-            if content[0] == "Jeeves":
-                text = [f"Good {greeting[0]} {self.title}. {greeting[1]}"]
+        if self.mode == "idle":
+            greeting = day_greeting()
 
-            if content[0] == "DM":
-                self.mode = "userSetTest"
-                text = [[f'Good {greeting[0]} {content[1]}. I am here to assist you',0],
-                        ["Would you please tell me your First and Last Name?",1],
-                        ["Are you Male (M) or Female (F)?",0],
-                        ["Please answer with a single Character",1]]
+            content = message.split(' ')
+            try:
+                if content[0] == "Jeeves":
+                    text = [f"Good {greeting[0]} {self.title}. {greeting[1]}"]
 
-            if content[0] == "Response":
-                self.mode = "responseTest"
-                text = [['This is a call and response test',0],
-                        ["What is response 1?",1],
-                        ["Response 2?",1],
-                        ["Pause for effect",0],
-                        ["Response 3?",1]]
+                if content[0] == "DM":
+                    self.mode = "userSetTest"
+                    text = [f'Good {greeting[0]} {content[1]}. I am here to assist you',
+                            "Would you please tell me your First and Last Name?",
+                            "Are you Male (M) or Female (F)?",
+                            "Please answer with a single Character"]
 
-            if content[0] == "roll":
-                if len(content) == 1:
-                    content.append("help")
-                text = [roller(content[1:])]
-                # for line in result:
-                #     text.append([str(line),0])
+                if content[0] == "Response":
+                    self.mode = "thinking"
+                    io = self.handleInput, self.handlePrint
+                    self._thread = Thread(target=responseTest, args = io, daemon=True)
+                    self._thread.start()
+                    self.mode = "idle"
 
+                if content[0] == "roll":
+                    if len(content) == 1:
+                        content.append("help")
+                    text = [roller(content[1:])]
 
-        except Exception as e:
-            text = [f"Something has gone wrong. Please ask again: {e}"]
+            except Exception as e:
+                text = [f"Something has gone wrong. Please ask again: {e}"]
+                print(e)
 
         for line in text:
-            self.output.put((line,self.interface,self.location))
+            self.queues[1].put((line,self.interface,self.location))
 
-    # def responseIn(self, answers):
-    #     """Creates a thread form processing answers."""
-    #     self.answers = answers
-    #     self._thread = Thread(target=self.responseProcess, daemon=True)
-    #     self._thread.start()
-    #     return "¯\\(°_o)/¯"
+    def handlePrint(self, message: str):
+        """Custom Print Statement to be passed to sub programs.
+        
+        Parameters:
+            self: TextHandler Instance
+            message: Message to be sent to the interface
+        """
+        self.queues[1].put((message,self.interface,self.location))
 
-    # def responseProcess(self):
-    #     """Test for response timing."""
-    #     process = self.mode
-    #     self.mode = "thinking"
+    def handleInput(self, message: str):
+        """Custom Input Statement to be passed to sub programs.
+        
+        Parameters:
+            self: TextHandler Instance
+            message: Message to be sent to the interface
 
-    #     if process == "responseTest":
-    #         print(self.answers)
-    #         for i in range(10):
-    #             print(i)
-    #             sleep(1)
-    #     self.mode = "waiting"
-
-    #     if process == "userSetTest":
-    #         print(self.answers)
-    #         for i in range(10):
-    #             print(i)
-    #             sleep(1)
-
-    #     sender(self, "Task Complete")
-    #     self.mode = "waiting"
-    #     return self
+        Returns:
+            response (str): Message recieved from the user
+        """
+        self.mode = "waiting"
+        self.queues[1].put((message,self.interface,self.location))
+        response = self.responseQueue.get()
+        self.mode = "thinking"
+        return response
 
 # Main Function
 def main():
     """Launches the TextHandler class and contains the run loop."""
-    textHandler = TextHandler()
+    queues = [queue.Queue(),queue.Queue()]
+    thread = Thread(target=TextHandler(queues = queues), daemon = True)
+    thread.start()
     while True:
-        answers = []
-        text = textHandler.messageIn(input())
-        for line in text:
-            if line[1] == 0:
-                print(line[0])
-            elif line[1] == 1:
-                answers.append(input(line[0]))
-        if len(answers) > 0:
-            endMsg = textHandler.responseIn(answers)
-            print(endMsg)
+        if not queues[1].empty():
+            print(queues[1].get()[0])
+        queues[0].put(input())
+        sleep(1)
 
 def day_greeting():
     """Gives a user response message based on the time of day."""
@@ -185,12 +193,18 @@ def day_greeting():
 
     return [time, greeting]
 
-def sender(self, message):
-    """Sends generated responses to the correct interface."""
-    if self.interface == "cmd":
-        print(message)
-    if self.interface == "Discord":
-        self.channel.send(message)
+def responseTest(handleIn, handleOut):
+    """Tests the Response processing of the text handler.
+    
+    Parameters:
+        handleIn (Input): Custom Input Statement for the Text Handler
+        handleOut (Print): Custom Output Statement for the Text Handler
+    """
+    handleOut("This is a call and response test")
+    handleIn("What is response 1?")
+    handleIn("What is response 2?")
+    handleOut("Pause for effect")
+    handleIn("Response 3?")
 
 # Self Program Call
 if __name__ == '__main__':
